da62bec375c1cdc3f79a1f8e30af7d8c6e3d722f
Leah Pronk <2109208@talnet.nl git config --global user.name Leah>
2019-11-15 13:41:47 +0100

    Uitwerking van de exercise

---
+++ name.php
@@ -0,0 +1,7 @@
+<?php
+
+$antwoord = readline ("wat is jouw voornaam?");
+echo "de gebruiker zegt" . '' . $antwoord;
+
+$antwoord2 = readline ("wat is je achternaam?");
+echo "de gebruiker zegt" . '' . $antwoord.$antwoord2;
